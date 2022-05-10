29bd55ad64b7c5b4a27b1c9da167ad2c61bbf232
unknown <simran.multani@ics-global.in>
2022-05-10 13:25:19 +0530

    Oops Project added

---
+++ OopsDemo/OopsDemo/Form1.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(146, 50)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Class and Object"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 78)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(146, 54)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Class Library"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 150)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(146, 49)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Method Overloading"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 214)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(146, 55)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Inheritance"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(185, 12)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(117, 50)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Method OverRiding"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(185, 78)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(117, 54)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "Interface"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(185, 150)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(117, 49)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "Extension"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(185, 214)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(117, 55)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "Pass By Value nd Pass by Ref"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(12, 284)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(146, 45)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "Thread1"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(185, 284)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(117, 45)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "Thread2"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(172, 365)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(236, 365)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Label2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ OopsDemo/OopsDemo/Form1.vb
@@ -0,0 +1,218 @@
+﻿Imports System.Runtime.CompilerServices
+Imports OopsDemoClassLibrary
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim t As Test
+        t = New Test
+        t.SayHello()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim p As New Person
+        p.FirstName = "Simran"
+        p.LastName = "Multani"
+        p.DateOfBirth = #10/03/2021#
+
+        MsgBox(p.FirstName & " " & p.LastName & " " & p.DateOfBirth)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim p As New Person
+
+        p.FirstName = "Simran"
+        p.LastName = "Multani"
+
+        If p.SaveDetails() Then
+            MsgBox("sucess!")
+        Else
+            MsgBox("error!")
+        End If
+
+        If p.SaveDetails("C:\Users\Public\Documents\simran1.txt") Then
+            MsgBox("sucess! 1 paramet method")
+        Else
+            MsgBox("error!  1 paramet method")
+        End If
+
+
+        If p.SaveDetails("C:\Users\Public\Documents\simran2.txt", "sm") Then
+            MsgBox("sucess! 2 paramet method")
+        Else
+            MsgBox("error!  2 paramet method")
+        End If
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim employeeDetail As New EmployeeDetail
+        Dim employee As New Employee
+
+        employee.SaveDetails()
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim polomorphism As New POlymorphism
+        Dim mul = polomorphism.Calculation(5, 6)
+        MsgBox("Multiplication of values =" & mul)
+
+        Dim p1 As New Person
+        Dim Sum = p1.Calculation(5, 6)
+        MsgBox("Sum of values =" & Sum)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim school As New School
+
+        school.Salary()
+        school.PeimaryDetails()
+        school.totalSubjects()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim example As String = "Example string"
+        example.Print()
+
+        example = "Hello"
+        example.PrintAndPunctuate(".")
+        example.PrintAndPunctuate("!!!!")
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim test As New Test
+
+        Const highRate As Double = 12.5
+        Dim lowRate = highRate * 0.6
+
+        Dim initialDebt = 4999.99
+
+        Dim debtWithInterest = initialDebt
+
+        test.Calculate(highRate, debtWithInterest)
+
+        Dim debtString = Format(debtWithInterest, "C")
+        MsgBox("high interest: " & debtString)
+
+        debtWithInterest = initialDebt
+        test.Calculate(lowRate, debtWithInterest)
+        debtString = Format(debtWithInterest, "C")
+        MsgBox("low INtrest" & debtString)
+    End Sub
+
+    Dim i As Integer
+    Dim i2 As Integer
+
+    Dim thread As System.Threading.Thread
+    Dim thread2 As System.Threading.Thread
+
+    Private Sub ContUp()
+        Do Until i = 1000
+            i = i + 1
+            Label1.Text = i
+            Me.Refresh()
+        Loop
+    End Sub
+
+    Private Sub ContUp2()
+        Do Until i = 1000
+            i2 = i + 1
+            Label2.Text = i2
+            Me.Refresh()
+        Loop
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        thread = New System.Threading.Thread(AddressOf ContUp)
+        thread.Start()
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        thread2 = New System.Threading.Thread(AddressOf ContUp2)
+        thread2.Start()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CheckForIllegalCrossThreadCalls = False
+    End Sub
+End Class
+
+
+
+Public Class Test
+    Public Sub Calculate(ByVal rate As Double, ByRef debt As Double)
+        debt = debt + (debt * rate / 100)
+    End Sub
+
+    Public Function SayHello()
+        MsgBox("Hello !")
+    End Function
+
+End Class
+
+Public Class School
+    Implements ITeacher
+    Implements IStudent
+
+    Public Sub Salary() Implements ITeacher.Salary
+        MsgBox("Salary called from Teacher")
+    End Sub
+
+    Public Sub PeimaryDetails() Implements IStudent.PrimaryDetails
+        MsgBox("Primary Details of students")
+    End Sub
+
+    Public Sub totalSubjects() Implements IStudent.totalSubjects
+        MsgBox("total Subjects from Subjects")
+    End Sub
+End Class
+
+
+Public Interface ISubjects
+    Sub totalSubjects()
+
+End Interface
+
+Public Interface ITeacher
+    Inherits ISubjects
+    Sub Salary()
+
+End Interface
+Public Interface IStudent
+    Inherits ISubjects
+    Sub PrimaryDetails()
+
+End Interface
+
+Public NotInheritable Class SealedClass
+    Private Sub New()
+
+    End Sub
+
+End Class
+
+Module ExtensionTest
+    <Extension()>
+    Public Sub Print(aString As String)
+        MsgBox(aString)
+    End Sub
+
+    <Extension()>
+    Public Sub PrintAndPunctuate(aString As String, punc As String)
+        MsgBox(aString & punc)
+    End Sub
+
+    Public Delegate Sub Sum(ByVal a As Integer)
+    Sub Main(ByVal args As String())
+
+        Dim y As Integer = 10
+        Dim Total As Sum = Sub(ByVal x As Integer)
+                               MsgBox("Total = " & x + y)
+                           End Sub
+
+
+    End Sub
+End Module
+
